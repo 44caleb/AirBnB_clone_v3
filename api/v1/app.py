90471ba8ada748eb9414cf10f0ac358180a3cc76
44caleb <calebadinfono2@gmail.com>
2023-10-30 12:19:33 +0000

    env variable

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,16 @@
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from models import storage
+import os
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes=False
 app.register_blueprint(app_views)
+
+
+api_host = os.getenv("HBNB_API_HOST")
+api_port = os.getenv("HBNB_API_PORT")
 
 
 @app.teardown_appcontext
@@ -23,4 +28,4 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", threaded=True)
+    app.run(host=api_host, port=api_port, threaded=True)
