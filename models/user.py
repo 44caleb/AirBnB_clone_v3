65a1a83584d2da841a4c49a4d74d9074502d6f89
44caleb <calebadinfono2@gmail.com>
2023-10-30 22:19:06 +0000

    hash

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,12 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        self.password = self.hash(self.password)
+
+    def hash(password):
+        """hashes user password"""
+        encoded_pass = password.encode("utf-8")
+        md5 = hashlib.md5()
+        md5.update(encoded_pass)
+        hashed_pass = md5.hexdigest()
+        return hashed_pass
